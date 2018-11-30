f2df6b8f8d3e712d151981bd2c25bc31cfb4ad6b
Christos Papazafeiropoulos <c.papazafeiropoulos@klarna.com>
2018-11-30 11:16:30 +0100

    Do not show any logs on production

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,11 +11,16 @@ module.exports = {
     watch: false,
     max_memory_restart: '1G',
     env: {
-      NODE_ENV: 'development',
-      DEBUG: true
+      NODE_ENV: 'production',
+      DEBUG: false
     },
     env_production: {
-      NODE_ENV: 'production'
+      NODE_ENV: 'production',
+      DEBUG: false
+    },
+    env_development: {
+      NODE_ENV: 'development',
+      DEBUG: true
     }
   }],
 };
